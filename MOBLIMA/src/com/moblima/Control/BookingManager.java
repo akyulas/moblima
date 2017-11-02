d7af9fe4f9ff5e4b41cdc6364fda35d13ec9afc8
bolsh <bolsh@DESKTOP-1T4N4Q0>
2017-11-03 00:47:35 +0800

    Added Population + various changes

---
+++ MOBLIMA/src/com/moblima/Control/BookingManager.java
@@ -0,0 +1,17 @@
+package com.moblima.Control;
+
+public class BookingManager implements Manager {
+
+	
+	
+	public void importData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void exportData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
